322a70d863c1d4dff99a0f704e8b01c3e88eb6e7
Kwadwo Owusu Ansah <owusuansahkwadwo24@gmail.com>
2023-07-27 19:24:53 +0000

    styling

--- src/components/FixedCard.jsx
+++ src/components/FixedCard.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "../styles/FixedCard.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-icons/fa/";
@@ -6,48 +5,58 @@ import { FaGithub, FaLinkedin, FaTwitter, FaInstagram } from "react-icons/fa";
 
 const FixedCard = () => {
   return (
-    <div className="fixed-card container-fluid card">
-      <h2>Kwadwo</h2>
-      <h4 className="card-text mt-1">Ethical Hacker & Web Developer</h4>
-      <img src="../assets/mypic.jpeg" alt="mypic" />
-      <p>Your Location</p>
-      <div className="social-icons">
-        <a
-          className="icon-link icon-link-hover"
-          href="https://github.com/hacks-and-codes/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaGithub />
-        </a>
-        <a
-          className="icon-link icon-link-hover"
-          href="https://linkedin.com/in/quarjo/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaLinkedin />
-        </a>
-        <a
-          className="icon-link icon-link-hover"
-          href="https://twitter.com/quarjowusu/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaTwitter />
-        </a>
-        <a
-          className="icon-link icon-link-hover"
-          href="https://www.instagram.com/quarjowusu/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaInstagram />
-        </a>
+    <div className="card container-fluid text-bg-dark p-3">
+      <h2 className="card-title ">Kwadwo</h2>
+      <div className="card-body">
+        <small className="text-body-secondary">
+          Ethical Hacker & Web Developer
+        </small>
       </div>
-      <a href="mailto:your@email.com" className="btn btn-success">
-        Email Me
-      </a>
+      <img src="src/assets/mypic.jpeg" alt="mypic" className="card-img-top" />
+      <p className="text-body-secondary">Kumasi</p>
+      <div className="social-icons">
+        <div className="icons-container icon-link-hover">
+          <a
+            className="icon-link icon-link-hover"
+            href="https://github.com/hacks-and-codes/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub className="icon" />
+          </a>
+        </div>
+        <div className="icons-container icon-link-hover">
+          <a
+            className="icon-link icon-link-hover"
+            href="https://linkedin.com/in/quarjo/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin className="icon" />
+          </a>
+        </div>
+        <div className="icons-container icon-link-hover">
+          <a
+            className="icon-link icon-link-hover"
+            href="https://twitter.com/quarjowusu/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaTwitter className="icon" />
+          </a>
+        </div>
+        <div className="icons-container icon-link-hover">
+          <a
+            className="icon-link icon-link-hover"
+            href="https://www.instagram.com/quarjowusu/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram className="icon" />
+          </a>
+        </div>
+      </div>
+      <button className="btn btn-success mt-3">Email me</button>
     </div>
   );
 };
