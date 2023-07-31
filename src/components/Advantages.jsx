e7aa6c57aa19141bbac39def1813b70c1fcbe0bf
hacks-and-codes <owusuansahkwadwo24@gmail.com>
2023-07-31 00:04:09 +0000

    styling

--- src/components/Advantages.jsx
+++ src/components/Advantages.jsx
@@ -1,21 +1,24 @@
-import { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Aos from "aos";
-
+import { FaPython, FaJs, FaLinux, FaReact, FaShieldAlt } from "react-icons/fa";
 
 const Advantages = () => {
-    useEffect(() => {
-        Aos.init({ duration: 1000 });
-    }, []);
+  return (
+    <div>
+      <div className="text-center card-body">
+        <i data-aos="fade-down" className="fa fa-tasks fa-1x fa-ico">
+            {" "}
+            SKills
+        </i>
+        <p className="text-body" data-aos="fade-up">
+        <FaJs />
+        <FaPython />
+        <FaLinux />
+        <FaReact />
+        <FaShieldAlt />
+        </p>
 
-    return (
-        <div>
-            <div className="text-center card-body">
-               
-               
-            </div>
-        </div>
-    );
-
+      </div>
+    </div>
+  );
 };
 export default Advantages;
--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -5,6 +5,11 @@ import { Carousel } from "react-bootstrap";
 const Certificates = () => {
   return (
     <div>
+      <div className="text-center card-body">
+        <i data-aos="fade" className="fa fa-certificate fa-1x fa-ico">
+         {" "} Certifications
+        </i>
+      </div>
       <Carousel>
         <Carousel.Item>
           <img
@@ -12,6 +17,18 @@ const Certificates = () => {
             className="d-block w-100"
             alt="cert-img"
           />
+          <Carousel.Caption>
+            <h3 className="text-body-secondary fs-4 fw-semibold">
+              <a
+                className="text-decoration-none"
+                href="https://www.coursera.org/verify/QP8P7YUJD7HH"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Verify
+              </a>
+            </h3>
+          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
@@ -19,6 +36,18 @@ const Certificates = () => {
             className="d-block w-100"
             alt="cert-img"
           />
+          <Carousel.Caption>
+            <h3 className="text-body-secondary fs-4 fw-semibold">
+              <a
+                className="text-decoration-none"
+                href="https://www.coursera.org/verify/XHYDSERR686T"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Verify
+              </a>
+            </h3>
+          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
@@ -26,6 +55,18 @@ const Certificates = () => {
             className="d-block w-100"
             alt="cert-img"
           />
+          <Carousel.Caption>
+            <h3 className="text-body-secondary fs-4 fw-semibold">
+              <a
+                className="text-decoration-none"
+                href="https://www.coursera.org/verify/9WR85FNZ6GHM"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Verify
+              </a>
+            </h3>
+          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
@@ -33,6 +74,37 @@ const Certificates = () => {
             className="d-block w-100"
             alt="cert-img"
           />
+          <Carousel.Caption>
+            <h3 className="text-body-secondary fs-4 fw-semibold">
+              <a
+                className="text-decoration-none"
+                href="https://www.coursera.org/verify/QQNSZJF4ZPCB"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Verify
+              </a>
+            </h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            src="src/assets/images/DME.png"
+            className="d-block w-100"
+            alt="cert-img"
+          />
+          <Carousel.Caption>
+            <h3 className="text-body-secondary fs-4 fw-semibold">
+              <a
+                className="text-decoration-none"
+                href="https://www.coursera.org/verify/UXT8LBK2MK34"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Verify
+              </a>
+            </h3>
+          </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
     </div>
