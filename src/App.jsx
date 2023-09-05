8b3c308fefb61feb10a613ad64237014314914df
hacks-and-codes <owusuansahkwadwo24@gmail.com>
2023-09-05 12:06:27 +0000

    testing failed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
 import "./App.css";
 import ContentCard from "./components/ContentCard.jsx";
-import { lazy, Suspense } from "react";
-
-const Certificates = lazy(() => import("./components/Certificates")); 
-const Projects = lazy(() => import("./components/Projects"));
 
 const App = () => {
   return (
     <div className="app">
-      <Suspense fallback={<div>Loading...</div>}>
-        <Certificates />
-        <Projects />
-      </Suspense>
       <div className="center">
         <ContentCard />
       </div>
