20e30f2855149ff95799b9d5f35bbf6bdf82c9e0
hacks-and-codes <owusuansahkwadwo24@gmail.com>
2023-09-07 15:55:04 +0000

    update

--- api/og.jsx
+++ api/og.jsx
@@ -1,49 +1,62 @@
-import { ImageResponse } from '@vercel/og';
- 
+import { ImageResponse } from "@vercel/og";
+
 export const config = {
-  runtime: 'edge',
+  runtime: "edge",
 };
- 
+
 export default function handler(request) {
-  const { searchParams } = request.url;
-  const username = searchParams.get('quarj0');
-  if (!username) {
-    return new ImageResponse(<>Visit with &quot;?quarj0=vercel&quot;</>, {
-      width: 1200,
-      height: 630,
-    });
-  }
- 
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          display: 'flex',
-          fontSize: 60,
-          color: 'black',
-          background: '#f6f6f6',
-          width: '100%',
-          height: '100%',
-          paddingTop: 50,
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <img
-          width="256"
-          height="256"
-          src={`https://github.com/${username}.png`}
+  try {
+    const { searchParams } = new URL(request.url);
+    const username = searchParams.get("quarj0");
+
+    if (!username) {
+      return new ImageResponse('Visit with "?quarj0=vercel"', {
+        width: 1200,
+        height: 630,
+      });
+    }
+
+    return new ImageResponse(
+      (
+        <div
           style={{
-            borderRadius: 128,
+            display: "flex",
+            fontSize: 60,
+            color: "black",
+            background: "#f6f6f6",
+            width: "100%",
+            height: "100%",
+            paddingTop: 50,
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
           }}
-        />
-        <p>github.com/{username}</p>
-      </div>
-    ),
-    {
-      width: 1200,
-      height: 630,
-    },
-  );
+        >
+          <img
+            width="256"
+            height="256"
+            src={`https://github.com/${username}.png`}
+            style={{
+              borderRadius: 128,
+            }}
+            alt={`${username}'s GitHub avatar`}
+          />
+          <p>github.com/{username}</p>
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    return new ImageResponse(
+      "An error occurred while processing your request.",
+      {
+        width: 1200,
+        height: 630,
+      }
+    );
+  }
 }
